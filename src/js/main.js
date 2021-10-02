00b47c287974ae32b29261561d7023a4d0e3d178
Aleksandr <alex.viun.arbeit@gmail.com>
2021-10-03 09:08:56 +1000

    big image

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
 import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use strice';
@@ -13,11 +14,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	changeModalState(modalState);
 
-	console.log(modalState);
 	modals();
 	tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 	tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 	tabs('.balcon_icons','.balcon_icons_img','.big_img > img', 'do_image_more','inline-block')
 	forms(modalState);
-	timer('.container', deadline);
+	timer('.container1', deadline);
+	images();
 });
